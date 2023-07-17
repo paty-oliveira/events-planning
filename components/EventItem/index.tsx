304296f5ed8938bb40dc9206e300fd3a90b28886
patricia-oliveira <patriciia.mota@gmail.com>
2023-07-17 15:30:11 +0100

    Add styling for EventItem component

--- components/EventItem/index.tsx
+++ components/EventItem/index.tsx
@@ -3,6 +3,9 @@ import Image from "next/image";
 import { EventItemProps } from "./types";
 import Link from "next/link";
 import { EventCard, EventLinkContainer } from "./styles";
+import { CiLocationOn } from "react-icons/ci";
+import { BsCalendarDate } from "react-icons/bs";
+import { BsSearch } from "react-icons/bs";
 
 const EventItem: React.FunctionComponent<EventItemProps> = ({ event }) => {
   const { id, title, location, date, image, isFeatured } = event;
@@ -17,10 +20,16 @@ const EventItem: React.FunctionComponent<EventItemProps> = ({ event }) => {
     <EventCard>
       <Image src={"/" + image} alt={title} width={"150"} height={"100"} />
       <h2>{title}</h2>
-      <h3>{readableDate}</h3>
-      <h4>{location}</h4>
+      <h3>
+        <BsCalendarDate className="icon" />
+        {readableDate}
+      </h3>
+      <h4>
+        <CiLocationOn className="icon"/>
+        {location}
+      </h4>
       <EventLinkContainer>
-        <Link href={`/events/${id}`}>Explore Event</Link>
+        <Link href={`/events/${id}`}><BsSearch className="icon"/>Explore Event</Link>
       </EventLinkContainer>
     </EventCard>
   );
--- components/EventItem/styles.tsx
+++ components/EventItem/styles.tsx
@@ -7,10 +7,35 @@ export const EventCard = styled.li`
     display: flex;
     flex-direction: column;
     justify-content: center;
-    align-items: center;
+    align-items: flex-start;
+    box-shadow: 0 1px 4px rgba(0, 0, 0, 0.3), 0 1px 12px 2px rgba(0, 0, 0, 0.2);
+    border-radius: 3%;
+
+    img {
+        object-fit: cover;
+        width: 100%;
+        height: 10rem;
+    }
+
+    h3 {
+        color: #443f40;
+        font-weight: bolder;
+    }
+
+    h4 {
+        color: #666666;
+        font-weight: bold;
+    }
+
+    .icon {
+        margin-right: 0.5rem;
+    }
 `;
 
 export const EventLinkContainer = styled.div`
+    width: 100%;
+    text-align: center;
+
     a {
         text-decoration: none;
         font-weight: bolder;
