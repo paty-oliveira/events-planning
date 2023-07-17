7537311ca150e80413b3922af1ca09e8fedf85d8
patricia-oliveira <patriciia.mota@gmail.com>
2023-07-17 15:53:17 +0100

    Replace hardcoded colors for theme colors

--- components/EventItem/styles.tsx
+++ components/EventItem/styles.tsx
@@ -8,8 +8,9 @@ export const EventCard = styled.li`
     flex-direction: column;
     justify-content: center;
     align-items: flex-start;
-    box-shadow: 0 1px 4px rgba(0, 0, 0, 0.3), 0 1px 12px 2px rgba(0, 0, 0, 0.2);
+    box-shadow: 0 1px 4px ${(props) => props.theme.colors["grey"]["000"] }, 0 1px 12px 2px ${(props) => props.theme.colors["grey"]["400"] };
     border-radius: 3%;
+    background-color: ${(props) => props.theme.colors["purple"]["000"]};
 
     img {
         object-fit: cover;
@@ -18,12 +19,12 @@ export const EventCard = styled.li`
     }
 
     h3 {
-        color: #443f40;
+        color: ${(props) => props.theme.colors["blue"]["800"]};
         font-weight: bolder;
     }
 
     h4 {
-        color: #666666;
+        color: ${(props) => props.theme.colors["blue"]["600"]};
         font-weight: bold;
     }
 
@@ -39,7 +40,7 @@ export const EventLinkContainer = styled.div`
     a {
         text-decoration: none;
         font-weight: bolder;
-        color: blue;
+        color: ${(props) => props.theme.colors["green"]["600"]};
         cursor: pointer;
     }
 `
--- theme/globalStyle.ts
+++ theme/globalStyle.ts
@@ -9,5 +9,6 @@ export const GlobalStyle = createGlobalStyle`
     body {
         font-family: ${poppins.style.fontFamily};
         margin: auto;
+        background-color: ${(props) => props.theme.colors["blue"]["000"]};
     }
 `
