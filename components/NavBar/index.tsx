78a0530d3de23536f2e29549ceac43459e05c03c
paty-oliveira <patriciia.mota@gmail.com>
2023-07-18 12:12:05 +0100

    Complement NavBar component

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { NavBarContainer } from "./styles";
+import Link from "next/link";
+import { FcPlanner } from "react-icons/fc";
 
-const NavBar:React.FunctionComponent = () => {
-    return (
-        <NavBarContainer>
-            <h2>Events Planning</h2>
-        </NavBarContainer>
-    )
-}
+const NavBar: React.FunctionComponent = () => {
+  return (
+    <NavBarContainer>
+      <h2>
+        <FcPlanner className="icon" /> Events Planning
+      </h2>
+      <Link href={"/events"}>All Events</Link>
+    </NavBarContainer>
+  );
+};
 
 export default NavBar;
--- components/NavBar/styles.tsx
+++ components/NavBar/styles.tsx
@@ -1,9 +1,28 @@
 import styled from "styled-components";
 
 export const NavBarContainer = styled.nav`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
   text-align: center;
   h2 {
     color: ${(props) => props.theme.colors["red"]["300"]};
     font-weight: bolder;
+    font-size: 1.5rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  a {
+    text-decoration: none;
+    font-weight: bolder;
+    color: ${(props) => props.theme.colors["red"]["300"]};
+    font-size: 1.5rem;
+  }
+
+  .icon {
+    margin-right: 0.2rem;
   }
 `;
