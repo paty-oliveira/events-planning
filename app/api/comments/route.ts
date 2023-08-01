9e0d52e5366e5a71722ae815e374decd5c4ac585
paty-oliveira <patriciia.mota@gmail.com>
2023-08-01 17:25:46 +0100

    Add draft implementation of getting comments from comments api

--- app/api/comments/route.ts
+++ app/api/comments/route.ts
@@ -1,7 +1,32 @@
 import { NextRequest, NextResponse } from "next/server";
 
+export type CommentsPayload = {
+  content: string;
+  author: string;
+};
+
+type GetCommentsResponse = {
+  status: number;
+  message: "Success" | "Failure";
+  comments: CommentsPayload[];
+};
+
 export async function GET() {
-  return NextResponse.json({ status: 200, message: "Success" });
+  const comments: CommentsPayload[] = [
+    {
+      content: "My comment is amazing!",
+      author: "Maximilian",
+    },
+    {
+      content: "Another comment ",
+      author: "Patricia",
+    },
+  ];
+  return NextResponse.json<GetCommentsResponse>({
+    status: 200,
+    message: "Success",
+    comments: comments,
+  });
 }
 
 export async function POST(request: NextRequest) {
--- components/CommentList/index.tsx
+++ components/CommentList/index.tsx
@@ -1,21 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { CommentsContainer, Comment } from "./styles";
+import { CommentsPayload } from "@/app/api/comments/route";
 
 const CommentList: React.FunctionComponent = () => {
+  const [comments, setComments] = useState<CommentsPayload[]>([
+    {
+      content: "My comment is amazing!",
+      author: "Maximilian",
+    },
+    {
+      content: "Another comment ",
+      author: "Patricia",
+    },
+  ]);
+
+  useEffect(() => {
+    async () => {
+      const response = await fetch("/api/comments", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const { comments } = await response.json();
+      setComments(comments);
+    };
+  }, []);
+
   return (
     <CommentsContainer>
-      <Comment>
-        <p>My Comment is amazing!</p>
-        <div>
-          By <address>Maximilian</address>
-        </div>
-      </Comment>
-      <Comment>
-        <p>My Comment is amazing!</p>
-        <div>
-          By <address>Maximilian</address>
-        </div>
-      </Comment>
+      {comments.map((comment, index) => (
+        <Comment key={index}>
+          <p>{comment.content}</p>
+          <div>
+            By <address>{comment.author}</address>
+          </div>
+        </Comment>
+      ))}
     </CommentsContainer>
   );
 };
