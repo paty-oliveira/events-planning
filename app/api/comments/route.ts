558cd67e629c47acb5d386e4fe113c58bc12288c
paty-oliveira <patriciia.mota@gmail.com>
2023-08-01 16:59:23 +0100

    Add draft API route for Comments

--- app/api/comments/route.ts
+++ app/api/comments/route.ts
@@ -1,12 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
 
+export async function GET() {
+  return NextResponse.json({ status: 200, message: "Success" });
+}
+
 export async function POST(request: NextRequest) {
   try {
-    const { emailAddress } = await request.json();
+    const { comment } = await request.json();
     return NextResponse.json({
       status: 201,
       message: "Success",
-      email: emailAddress,
+      comment: comment,
     });
   } catch (e) {
     return NextResponse.json({ error: e }, { status: 500 });
